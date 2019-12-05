2be3ae6881d0ffa8c4f7936f90fb44edff16c6f0
Zachary Jacobs <skatboyza124@hotmail.com>
2019-12-05 17:02:10 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -52,7 +52,6 @@ def reduce_to_total(source_array, starting_point=0)
   index = 0 
   total = starting_point
   while index < source_array.length do
-  if source_array[index]
     total += source_array[index]
   end
   index += 1 
