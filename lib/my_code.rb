faef0e24b878024985df088b868d0a835ce05634
Zachary Jacobs <skatboyza124@hotmail.com>
2019-12-05 17:13:42 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -62,10 +62,10 @@ end
 def reduce_to_all_true(source_array)
   index = 0 
   while index < source_array.length do
-    if source_array[index] == true 
-      return truthy
-    else 
-      return false
+    if source_array[index] !== true 
+ return false
+ else
+   return true
     end
       index += 1
   end
